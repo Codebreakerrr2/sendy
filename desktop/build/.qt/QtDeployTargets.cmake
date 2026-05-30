5c60d6e90447740ea9c8dfc9ee8a1bf154b57114
usman <Usman.amini@icloud.com>
2026-05-30 16:28:59 +0200

    chore: cmake build-ordner aus dem git-tracking entfernt

--- desktop/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_sendy_FILE /Users/usmanamini/Documents/sendy/desktop/build/sendy)
-set(__QT_DEPLOY_TARGET_sendy_TYPE EXECUTABLE)
--- desktop/build/CMakeFiles/sendy.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for sendy.
-# This may be replaced when dependencies are built.
